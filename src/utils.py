45da8dd09e29f5dfae9d704540ad0855d7b71f7f
Lola <lola@egherman.com>
2022-02-26 01:28:53 -0600

    use the right function

--- src/utils.py
+++ src/utils.py
@@ -5,5 +5,5 @@ est = timezone('America/New_York')
 
 
 def format_date(date: datetime) -> str:
-    date = est.localize(date)
+    date = date.astimezone(est)
     return date.strftime("%a, %b %d %Y at %I:%M %p %Z")
